62de269d9e98f6a241606a6ecf95e6d4e7f8396e
Alexander Vansach <vandamm@users.noreply.github.com>
2023-05-08 17:47:43 +0200

    Add missing imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ParcelService } from './services/parcel/parcel.service';
+import { RepositoriesModule } from './repositories/repositories.module';
 
-@Module({})
+@Module({
+  imports: [RepositoriesModule],
+  providers: [ParcelService],
+})
 export class AppModule {}
